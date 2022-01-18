53635fa631e7d92cc12b4a68d13eba5c758652c9
Andreas Paul <xorpaul@gmail.com>
2022-01-18 14:42:29 +0100

    use request URI as cache file instead of hashsum

--- cache.go
+++ cache.go
@@ -3,63 +3,77 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"crypto/sha256"
-	"encoding/hex"
 	"fmt"
-	"hash"
 	"io"
 	"io/ioutil"
 	"os"
+	"path/filepath"
 	"regexp"
+	"strings"
 	"sync"
 	"time"
 
-	"github.com/hauke96/sigolo"
+	h "github.com/xorpaul/gohelper"
+	olo "github.com/xorpaul/sigolo"
 )
 
 type Cache struct {
-	folder      string
-	hash        hash.Hash
-	knownValues map[string]KnownValues
-	busyValues  map[string]*sync.Mutex
-	mutex       *sync.Mutex
+	folder           string
+	busyItems        map[string]*sync.Mutex
+	cacheMemoryItems map[string]CacheMemoryItem
+	mutex            *sync.Mutex
 }
 
-type KnownValues struct {
+type CacheMemoryItem struct {
 	loadedAt time.Time
 	content  []byte
 }
 
-func CreateCache(path string) (*Cache, error) {
-	fileInfos, err := ioutil.ReadDir(path)
-	if err != nil {
-		sigolo.Error("Cannot open cache folder '%s': %s", path, err)
-		sigolo.Info("Create cache folder '%s'", path)
-		os.Mkdir(path, os.ModePerm)
-	}
+type CacheResponse struct {
+	loadedAt time.Time
+	content  io.ReadSeeker
+}
 
-	values := make(map[string]KnownValues, 0)
+func CreateCache(cacheFolder string) (*Cache, error) {
+	cacheFolder = h.CheckDirAndCreate(cacheFolder, "CreateCache")
+
 	busy := make(map[string]*sync.Mutex, 0)
+	memory := make(map[string]CacheMemoryItem, 0)
 
 	// Go through every file an save its name in the map. The content of the file
 	// is loaded when needed. This makes sure that we don't have to read
 	// the directory content each time the user wants data that's not yet loaded.
-	for _, info := range fileInfos {
-		if !info.IsDir() {
-			values[info.Name()] = KnownValues{}
+	prefillCache := func(path string, info os.FileInfo, err error) error {
+		if h.IsDir(path) || path == cacheFolder {
+			return nil
 		}
+		// removing cache dir from path
+		olo.Debug("path: %s", path)
+		cachedItem := strings.TrimPrefix(path, cacheFolder)
+		memory[cachedItem] = CacheMemoryItem{}
+		olo.Debug("adding to cache: %s", cachedItem)
+
+		return nil
 	}
 
-	hash := sha256.New()
+	c := make(chan error)
+	olo.Debug("filepath.Walk'ing directory " + cacheFolder)
+	go func() { c <- filepath.Walk(cacheFolder, prefillCache) }()
+	<-c // Walk done
+
+	// for _, info := range fileInfos {
+	// if !info.IsDir() {
+	// memory[info.Name()] = KnownValues{}
+	// }
+	// }
 
 	mutex := &sync.Mutex{}
 
 	cache := &Cache{
-		folder:      path,
-		hash:        hash,
-		knownValues: values,
-		busyValues:  busy,
-		mutex:       mutex,
+		folder:           cacheFolder,
+		busyItems:        busy,
+		mutex:            mutex,
+		cacheMemoryItems: memory,
 	}
 
 	return cache, nil
@@ -70,16 +84,14 @@ func CreateCache(path string) (*Cache, error) {
 // the resource is not found, a lock indicating that the resource is busy will
 // be returned. Once the resource has been put into cache the busy lock *must*
 // be unlocked to allow others to access the newly cached resource
-func (c *Cache) has(key string) (*sync.Mutex, bool) {
-	hashValue := calcHash(key)
-
+func (c *Cache) has(requestedURL string) (*sync.Mutex, bool) {
 	c.mutex.Lock()
 	defer c.mutex.Unlock()
 
 	// If the resource is busy, wait for it to be free. This is the case if
 	// the resource is currently being cached as a result of another request.
 	// Also, release the lock on the cache to allow other readers while waiting
-	if lock, busy := c.busyValues[hashValue]; busy {
+	if lock, busy := c.busyItems[requestedURL]; busy {
 		c.mutex.Unlock()
 		lock.Lock()
 		lock.Unlock()
@@ -88,7 +100,7 @@ func (c *Cache) has(key string) (*sync.Mutex, bool) {
 
 	// If a resource is in the shared cache, it can't be reserved. One can simply
 	// access it directly from the cache
-	if _, found := c.knownValues[hashValue]; found {
+	if _, found := c.cacheMemoryItems[requestedURL]; found {
 		return nil, true
 	}
 
@@ -96,102 +108,103 @@ func (c *Cache) has(key string) (*sync.Mutex, bool) {
 	// been cached successfully. Unlocking lock is required!
 	lock := new(sync.Mutex)
 	lock.Lock()
-	c.busyValues[hashValue] = lock
+	c.busyItems[requestedURL] = lock
 	return lock, false
 }
 
-func (c *Cache) get(requestedURL string) (*io.Reader, error) {
-	var response io.Reader
+func (c *Cache) get(requestedURL string) (CacheResponse, error) {
 	cacheURL, err := removeSchemeFromURL(requestedURL)
 	if err != nil {
-		return nil, err
+		return CacheResponse{}, err
 	}
-	hashValue := calcHash(cacheURL)
-
 	// Try to get content. Error if not found.
 	c.mutex.Lock()
-	KnownValue, ok := c.knownValues[hashValue]
-	content := KnownValue.content
+	cacheMemoryItem, ok := c.cacheMemoryItems[cacheURL]
 	c.mutex.Unlock()
-	if !ok && len(content) > 0 {
-		sigolo.Debug("Cache doesn't know key '%s'", hashValue)
-		return nil, fmt.Errorf("Key '%s' is not known to cache", hashValue)
+
+	if !ok && len(cacheMemoryItem.content) > 0 {
+		olo.Debug("Cache item not found: '%s'", cacheURL)
+		return CacheResponse{}, fmt.Errorf("cache item '%s' is not known", cacheURL)
 	}
 
-	sigolo.Debug("requested URL '%s' has cache key '%s'", requestedURL, hashValue)
+	// check if Cache is too old based on mtime, if so call getRemote() and renew cache
+	err = checkCacheTTL(c.folder+cacheURL, cacheURL, requestedURL)
+	if err != nil {
+		return CacheResponse{}, err
+	}
 
-	// Key is known, but not loaded into RAM
-	if content == nil {
-		sigolo.Debug("Cache item '%s' known but is not stored in memory. Reading from file.", hashValue)
+	// Key is known, but not found in-memory, read from file
+	if cacheMemoryItem.content == nil {
+		olo.Debug("Cache item '%s' known but is not stored in memory. Reading from file: %s", cacheURL, filepath.Join(c.folder, cacheURL))
 
-		// check if Cache is too old based on mtime, if so call getRemote() and renew cache
-		err := checkCacheTTL(c.folder+hashValue, cacheURL, requestedURL)
+		file, err := os.Open(filepath.Join(c.folder, cacheURL))
 		if err != nil {
-			return nil, err
-		}
-
-		file, err := os.Open(c.folder + hashValue)
-		if err != nil {
-			sigolo.Error("Error reading cached file '%s': %s", hashValue, err)
-			return nil, err
+			olo.Error("Error reading cached file '%s': %s", cacheURL, err)
+			return CacheResponse{}, err
 		}
 
 		fi, err := file.Stat()
 		if err != nil {
-			sigolo.Error("Error stating cached file '%s': %s", hashValue, err)
-			return nil, err
+			olo.Error("Error stating cached file '%s': %s", cacheURL, err)
+			return CacheResponse{}, err
 		}
+		// TODO: neede by http.ServeContent otherwise:
+		// seeker can't seek
+		// file already closed
+		// defer file.Close()
 
-		response = file
 		promSummaries["CACHE_READ_FILE"].Observe(float64(fi.Size()))
+		return CacheResponse{content: file, loadedAt: cacheMemoryItem.loadedAt}, nil
 
-	} else { // Key is known and data is already loaded to RAM
-		// check if Cache is too old based on mtime, if so call getRemote() and renew cache
-		err := checkCacheTTL(c.folder+hashValue, cacheURL, requestedURL)
-		if err != nil {
-			return nil, err
-		}
-		response = bytes.NewReader(content)
-		promSummaries["CACHE_READ_MEMORY"].Observe(float64(len(content)))
 	}
 
-	return &response, nil
+	// Key is known and data is already loaded to RAM
+	promSummaries["CACHE_READ_MEMORY"].Observe(float64(len(cacheMemoryItem.content)))
+	content := bytes.NewReader(cacheMemoryItem.content)
+	return CacheResponse{content: content, loadedAt: cacheMemoryItem.loadedAt}, nil
+
 }
 
 // release is an internal method which atomically caches an item and unmarks
 // the item as busy, if it was busy before. The busy lock *must* be unlocked
 // elsewhere!
-func (c *Cache) release(hashValue string, content []byte, loadedAt time.Time) {
+func (c *Cache) release(requestedURL string, content []byte, loadedAt time.Time) {
 	c.mutex.Lock()
-	delete(c.busyValues, hashValue)
-	c.knownValues[hashValue] = KnownValues{content: content, loadedAt: loadedAt}
+	delete(c.busyItems, requestedURL)
+	c.cacheMemoryItems[requestedURL] = CacheMemoryItem{content: content, loadedAt: loadedAt}
 	c.mutex.Unlock()
 }
 
-func (c *Cache) put(key string, content *io.Reader, contentLength int64) error {
-	hashValue := calcHash(key)
+func (c *Cache) put(cacheURL string, content *io.Reader, contentLength int64) error {
+	// make sure cache directories exist
+	fileCacheDir := filepath.Dir(filepath.Join(c.folder, cacheURL))
+	_ = h.CheckDirAndCreate(fileCacheDir, "Cache.put")
 
-	// Small enough to put it into the in-memory cache
 	if contentLength <= config.MaxCacheItemSize*1024*1024 {
+		// Small enough to put it into the in-memory cache
 		buffer := &bytes.Buffer{}
 		_, err := io.Copy(buffer, *content)
 		if err != nil {
 			return err
 		}
 
-		defer c.release(hashValue, buffer.Bytes(), time.Now())
-		sigolo.Debug("Added %s into in-memory cache", hashValue)
+		defer c.release(cacheURL, buffer.Bytes(), time.Now())
+		olo.Debug("Added %s into in-memory cache", cacheURL)
 
-		err = ioutil.WriteFile(c.folder+hashValue, buffer.Bytes(), 0644)
+		err = ensureDir(filepath.Join(c.folder, cacheURL))
 		if err != nil {
 			return err
 		}
-		sigolo.Debug("Wrote content of entry %s into file", hashValue)
-	} else { // Too large for in-memory cache, just write to file
-		defer c.release(hashValue, nil, time.Now())
-		sigolo.Debug("Added nil-entry for %s into in-memory cache", hashValue)
+		err = ioutil.WriteFile(c.folder+cacheURL, buffer.Bytes(), 0644)
+		if err != nil {
+			return err
+		}
+		olo.Debug("Wrote content of entry %s into file", cacheURL)
+	} else {
+		// Too large for in-memory cache, just write to file
+		defer c.release(cacheURL, nil, time.Now())
 
-		file, err := os.Create(c.folder + hashValue)
+		file, err := os.Create(c.folder + cacheURL)
 		if err != nil {
 			return err
 		}
@@ -201,55 +214,65 @@ func (c *Cache) put(key string, content *io.Reader, contentLength int64) error {
 		if err != nil {
 			return err
 		}
-		sigolo.Debug("Wrote content of entry %s into file", hashValue)
+		olo.Debug("Wrote content of entry %s into file", cacheURL)
 	}
 
-	sigolo.Debug("Cache wrote content into '%s'", hashValue)
+	olo.Debug("Cache wrote content into '%s'", cacheURL)
 
 	return nil
-}
-
-func calcHash(data string) string {
-	sha := sha256.Sum256([]byte(data))
-	return hex.EncodeToString(sha[:])
 }
 
 func checkCacheTTL(filePath string, cacheURL string, requestedURL string) error {
 	fi, err := os.Stat(filePath)
 	if err != nil {
-		return err
+		promCounters["CACHE_ITEM_MISSING"].Inc()
+		_, err := GetRemote(requestedURL)
+		if err != nil {
+			return err
+		}
+		olo.Error("found cache item while starting service, but it was removed afterwards, trying to get it again: '%s'", requestedURL)
+		err = checkCacheTTL(filePath, cacheURL, requestedURL)
+		if err != nil {
+			return err
+		}
+		return nil
 	}
 	mtime := fi.ModTime()
 
 	ttl := config.DefaultCacheTTL
 	for name, cr := range config.CacheRules {
 		r := regexp.MustCompile(cr.Regex)
-		// sigolo.Debug("comparing regex rule: '%s' with regex '%s' with cacheURL: '%s'", name, cr.Regex, cacheURL)
+		// olo.Debug("comparing regex rule: '%s' with regex '%s' with cacheURL: '%s'", name, cr.Regex, cacheURL)
 		if r.MatchString(cacheURL) {
-			sigolo.Debug("found matching regex rule: '%s' with regex '%s' and ttl '%s' for cacheURL: '%s'", name, cr.Regex, cr.TTL, cacheURL)
+			olo.Debug("found matching regex rule: '%s' with regex '%s' and ttl '%s' for cacheURL: '%s'", name, cr.Regex, cr.TTL, cacheURL)
 			ttl = cr.TTL
-			// sigolo.Debug("setting ttl to '%s' for file '%s'", ttl, cacheURL)
+			// olo.Debug("setting ttl to '%s' for file '%s'", ttl, cacheURL)
 			break
 		}
 	}
 
-	sigolo.Debug("using cache TTL '%s' for file: '%s'", ttl, cacheURL)
+	olo.Debug("using cache TTL '%s' for file: '%s'", ttl, cacheURL)
 	validUntil := mtime.Add(ttl)
 
 	//valid := time.Now().AddDate(1, 0, 0)
 	//fmt.Println(validUntil)
-	// sigolo.Info("cacheURL:", cacheURL)
-	// sigolo.Info("requestedURL:", requestedURL)
+	// olo.Info("cacheURL:", cacheURL)
+	// olo.Info("requestedURL:", requestedURL)
 	if time.Now().After(validUntil) {
-		sigolo.Info("CACHE_TOO_OLD for requested URL '%s'", cacheURL)
+		olo.Info("CACHE_TOO_OLD for requested URL '%s'", cacheURL)
 		promCounters["CACHE_TOO_OLD"].Inc()
-		err := GetRemote(requestedURL)
+		_, err := GetRemote(requestedURL)
 		if err != nil {
-			return err
+			if config.ReturnCacheIfRemoteFails {
+				olo.Info("checking if remote " + requestedURL + " has a different/newer version failed, so provide the cached item as a fallback")
+				return nil
+			} else {
+				return err
+			}
 		}
 		return nil
 	}
-	sigolo.Info("CACHE_OK until '%s'/'%s' for requested URL '%s'", time.Until(validUntil), validUntil.Format("2006-01-02 15:04:05"), cacheURL)
+	olo.Info("CACHE_OK until '%s'/'%s' for requested URL '%s'", time.Until(validUntil), validUntil.Format("2006-01-02 15:04:05"), cacheURL)
 	promCounters["CACHE_OK"].Inc()
 	return nil
 }
